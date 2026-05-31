c7e76ad972a4c241db610939e80ec0081d1d5008
m1stwng <m1stwng@gmail.com>
2026-05-31 11:19:44 -0300

    initial commit

---
+++ src/test/java/dev/m1stwng/nebula/NebulaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.m1stwng.nebula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NebulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
